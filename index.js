f9cbb16e7b9e2a9caa45b1269bcd20eb405f0a9e
gauri-97 <ggauri97@gmail.com>
2019-03-18 23:37:51 +0530

    Made the logs page

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express= require('express')
 const joi=require('joi')
 const moment=require('moment')
 const fs=require('fs')
-const schedule = require('node-schedule');
+ var msglog={}
 
 var app=express();
 var port=3000;
@@ -11,7 +11,7 @@ app.use(express.urlencoded({extended:true}));
 app.set('views', './views');
 app.set('view engine','pug');
 
-var secrets = {}
+
 var file = 'secrets.txt';
 var data= '';
 var phone;
@@ -26,7 +26,7 @@ var arr=data.toString().split('\r\n')
 var accountSid=arr[0];
 var authToken=arr[1];
 var sender=arr[2];
-
+var recv=arr[3];
 
 app.get('/',(req,res)=>{
     res.render('home');
@@ -45,16 +45,22 @@ app.post('/details',(req,res)=>{
         phone=result.value.phone;
         //will call the send sms function every hour
         var now=moment().format('HH:mm');
-        if(now >result.value.wake && now < result.value.sleep ){
-            setInterval(sendsms,3600000 );
+        if(now > result.value.wake && now < result.value.sleep ){
+            setInterval(sendsms,10000 );
+        }
+        else{
+            res.send("Sleeping time");
         }
         res.send(result.value)
     }
 })
-// Make a page that takes:
+app.get('/logs',(req,res)=>{
+    var time = moment(start).fromNow(true);
+    console.log(msglog);
+    res.render('logs',{time,msglog});
+});
 
-
-//Make another page that show's the time the app has been running and log of sent and failed messages
+//log of sent and failed messages
 
 
 
@@ -68,35 +74,42 @@ app.post('*',(req,res)=>{
 
 
 
-
-
-
 function sendsms(phone){
-    var cnt=0; var flag='not'
+    var cnt=1; var flag='not'
     const client = require('twilio')(accountSid, authToken);
     client.messages.create({
         body: 'Hey , your name is John!',
         from: sender,
-        to: phone,
+        to: recv,
         statusCallback: 'http://postb.in/1234abcd',
     })
     .then(
         message => {console.log(message.sid,message.status)
+
+            var msgtime=moment().format('HH:mm');
+            var arr=[message.status,msgTime];
+            msglog[message.sid]=arr;
+
             if(message.status =='failed' ||message.status =='undelivered'){
-                while(cnt<5 && flag!='sent'){
+                while(cnt<=5 && flag!='sent'){
                     client.messages.create({
                     body: 'Hey , your name is John!',
                     from: sender,
-                    to: phone,
+                    to: recv,
                     statusCallback: 'http://postb.in/1234abcd'
                     })
                     .then(
                         message => {console.log(message.sid)
+                        var msgtime=moment().format('HH:mm');
                         if(message.status === 'delivered' ||message.status === 'sent' ||message.status === 'queued'){
                             flag='sent';
                             }
                             cnt++;
+                            console.log(cnt);
+                            var arr=[message.status,msgTime];
+                            msglog[message.sid]=arr;
                         }
+                        
                     );
                 }
             }
@@ -107,6 +120,6 @@ function test(){
     console.log("hey");
 }
 app.listen(port)
-start=moment().format('HH:mm')
+start=moment().format('YYYY-MM-DDTHH:mm:ss')
 console.log("Server is up and running on Port :",port);
 
